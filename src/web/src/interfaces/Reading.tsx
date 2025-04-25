42431b8a14407201245bd9792165c2234d3fca6d
sintutu <160270685+sintutu@users.noreply.github.com>
2025-04-26 01:59:54 +0200

    refactor: :recycle: Declutter UI
    Remove H1 heading, Move Theme, Update copy to standard format

--- src/web/src/interfaces/Reading.tsx
+++ src/web/src/interfaces/Reading.tsx
@@ -1,9 +1,9 @@
 export interface Reading {
-    theme: string;
-    title: string;
-    quote: string;
-    author: string;
-    citation: string;
-  }
+  theme: string;
+  title: string;
+  quote: string;
+  author: string;
+  citation: string;
+}
 
 export type Readings = Record<string, Reading>;

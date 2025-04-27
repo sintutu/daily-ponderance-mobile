04c717c20085284f62877676fdfb8e3913dd9393
sintutu <160270685+sintutu@users.noreply.github.com>
2025-04-27 17:51:37 +0200

    feat: :sparkles: Use Em dash

--- src/web/src/pages/DailyReading.tsx
+++ src/web/src/pages/DailyReading.tsx
@@ -68,14 +68,14 @@ const DailyReading: React.FC = () => {
         <div style={{ marginTop: '1rem', border: '1px solid #ccc', padding: '1rem', borderRadius: '8px' }}>
           <h3>{reading.title}</h3>
           <blockquote>
-            <p>"{reading.quote}"</p>
+            <p>"{reading.quote.replace(/--/g, "\u2014")}"</p>
           </blockquote>
           <footer>- {reading.author}, <em>{reading.citation}</em></footer>
           {/* Add the CopyButton here */}
           <CopyButton
             date={date}
             title={reading.title}
-            quote={reading.quote}
+            quote={reading.quote.replace(/--/g, "\u2014")}
             author={reading.author}
             citation={reading.citation}
           />
